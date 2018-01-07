4d33a0bcfe665389e52f64a8fe911c55dc51803c
Arthur Langereis <puurtuur@me.com>
2018-01-07 15:02:19 -0700

    quick post creation script
    use `node post <title>`

---
+++ post.js
@@ -0,0 +1,27 @@
+// @ts-check
+const fs = require("fs");
+const spawn = require("child_process").spawn;
+
+const title = process.argv.slice(2).join(" ") || "Untitled";
+const fileTitle = title.split(" ").map(s => s.toLowerCase().replace(/[\\\/\?\!\-'",\.\t]/g, "")).filter(s => s.length > 0).join("-");
+
+const now = new Date();
+const date = now.toISOString().replace("T", " ").replace("Z", " +0000").replace(/\.\d\d\d/, "");
+const paddedMonth = `0${now.getUTCMonth() + 1}`.substr(-2);
+const paddedDay = `0${now.getUTCDate()}`.substr(-2);
+
+const fileName = `${now.getUTCFullYear()}-${paddedMonth}-${paddedDay}-${fileTitle}.md`;
+const filePath = `_posts/${fileName}`;
+
+const header = `---
+layout: post
+title:  "${title}"
+date:   ${date}
+---
+
+`;
+
+fs.writeFile(filePath, header, (err) => {
+	if (err) throw err;
+	spawn("open", [filePath]);
+});
